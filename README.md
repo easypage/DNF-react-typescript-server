34b3c9773c4359ee9f0911711d4db4e625a10c85
EasyPage <74344815+easypage@users.noreply.github.com>
2022-09-05 13:00:24 +0900

    리드미 작성

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
 # DNF-react-typescript-server
+
+# REST API
+
+## 기본 도메인
+
+> https://dnf-react-typescript.herokuapp.com/
+
+#
+
+## Character
+
+#
+
+### nickNameSearch
+
+> 서버에서 닉네임을 검색 합니다.
+
+> Get
+
+> 파라미터 nickname(한글)
+
+    https://dnf-react-typescript.herokuapp.com/character/nickname?nickname=<닉네임>
+
+#### 예시
+
+     https://dnf-react-typescript.herokuapp.com/character/nickname?nickname=메론맛어썰트
+
+#### 결과예시
+
+    {"rows":[{"serverId","characterId","characterName","level":110,"jobId","jobGrowId","jobName","jobGrowName"}]}
+
+    #
+
+### defaultSearch
+
+> 캐릭터 아이디로 상세 검색합니다.
+
+> Get
+
+> 파라미터 serverId, charcterId(닉네임검색 value)
+
+    https://dnf-react-typescript.herokuapp.com/character/default?serverId=<서버아이디>&characterId=<캐릭터아이디>
+
+#### 예시
+
+     https://dnf-react-typescript.herokuapp.com/character/default?serverId=prey&characterId=9ea844d6c6a69c6770c3dadb2f413bf1
+
+#### 결과예시
+
+    {character:{},"equipment": [],  "avatar":[],  "creature":{},"talismans":[],}
+
+    #
+
+### characterImage
+
+> 캐릭터카드 이미지를 줍니다.
+
+> Get (리다이렉트)
+
+> 파라미터 serverId, charcterId(닉네임검색 value)
+
+    https://dnf-react-typescript.herokuapp.com/character/characterImage?serverId=<서버아이디>&characterId=<캐릭터아이디>
+
+#### 예시
+
+https://dnf-react-typescript.herokuapp.com/character/characterImage?serverId=prey&characterId=9ea844d6c6a69c6770c3dadb2f413bf1
+
+#### 결과 예시
+
+![캐릭터이미지 API 예결과](/readmeAsset/%EC%BA%90%EB%A6%AD%ED%84%B0%EC%9D%B4%EB%AF%B8%EC%A7%80.png)
--- Router/Character.js
+++ Router/Character.js
@@ -1,5 +1,9 @@
 const express = require("express"); // express 임포트
-const { defaultSearch, nickNameSearch, characterImage } = require("../API/CharacterAPI.js");
+const {
+  defaultSearch,
+  nickNameSearch,
+  characterImage,
+} = require("../API/CharacterAPI.js");
 const router = express.Router();
 
 router.use(function (req, res, next) {
@@ -7,15 +11,11 @@ router.use(function (req, res, next) {
 });
 
 router.get("/", function (req, res) {
-
-
-res.send("캐릭터 페이지입니다.")
+  res.send("캐릭터 페이지입니다.");
 });
-
 
 // 닉네임 검색
 router.get("/nickName", async function (req, res) {
-  
   const nickname = req.query.nickname;
 
   if (!nickname) {
@@ -23,40 +23,38 @@ router.get("/nickName", async function (req, res) {
   }
 
   const api = await nickNameSearch(nickname);
-  if (api.error) { 
+  if (api.error) {
     res.status(500).json(api);
   }
   res.status(200).json(api);
-  });
-  
+});
+
 // 경로로 받기 체크
 
 router.get("/default", async function (req, res) {
   const { serverId, characterId } = req.query;
-// 파라미터 확인
-  if (!serverId|!characterId) {
-    return res.status(404).json({error: ["400", "Bad Request"]})
+  // 파라미터 확인
+  if (!serverId | !characterId) {
+    return res.status(404).json({ error: ["400", "Bad Request"] });
   }
 
   const api = await defaultSearch(serverId, characterId);
- // api호출 오류 확인
-  if (api.error) { 
-   return res.status(501).json(api);
+  // api호출 오류 확인
+  if (api.error) {
+    return res.status(501).json(api);
   }
-  
+
   return res.status(200).json(api);
 });
-
 
 router.get("/characterimage", function (req, res) {
   const { serverId, characterId } = req.query;
   // 파라미터 확인
-    if (!serverId|!characterId) {
-      return res.status(404).json({error: ["400", "Bad Request"]})
-    }
+  if (!serverId | !characterId) {
+    return res.status(404).json({ error: ["400", "Bad Request"] });
+  }
 
-  res.redirect(characterImage(serverId,characterId))
-
- });
+  res.redirect(characterImage(serverId, characterId));
+});
 
 module.exports = { CharcterRouter: router };
