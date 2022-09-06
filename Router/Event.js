2d452a5ef5046a4d473f58a4b285324227600dd5
EasyPage <74344815+easypage@users.noreply.github.com>
2022-09-06 16:56:47 +0900

    eventbanner추가

---
+++ Router/Event.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const dir = "./public/eventBanner";
+
+const express = require("express"); // express 임포트
+const router = express.Router();
+
+router.use(function (req, res, next) {
+  next();
+});
+
+router.get("/", async function (req, res) {
+  res.send("이벤트 페이지입니다.");
+});
+
+router.get("/banner", async function (req, res) {
+  let bannerlist = [];
+  await fs.readdirSync(dir).forEach((file) => {
+    bannerlist.push(file);
+  });
+
+  res.send(bannerlist);
+});
+
+module.exports = { EventRouter: router };
--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express = require("express"); // express 임포트
 const app = express(); // app생성
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+
 const { CharcterRouter } = require("./Router/Character");
 const { ItemRouter } = require("./Router/Item");
+const { EventRouter } = require("./Router/Event");
 
 app.listen(PORT);
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/character", CharcterRouter);
 app.use("/item", ItemRouter);
+app.use("/event", EventRouter);
 
-app.get("/", function (req, res) {
-  res.send("기본페이지입니다.");
-});
+app.get("/", function (req, res) {});
