5788599feed70e0db2bee8f3f3f6724bef5198b2
EasyPage <74344815+easypage@users.noreply.github.com>
2022-09-13 13:09:36 +0900

    update event png

--- API/Event/EventBannerLIst.js
+++ API/Event/EventBannerLIst.js
@@ -1,7 +1,11 @@
 const eventList = [
   {
-    link: "https://df.nexon.com/df/pg/chuseok2022",
+    url: "https://df.nexon.com/df/pg/chuseok2022",
     img: "https://dnf-react-typescript.herokuapp.com/eventBanner/오늘의던전.png",
+  },
+  {
+    url: "https://df.nexon.com/df/pg/landrunner2022",
+    img: "https://dnf-react-typescript.herokuapp.com/eventBanner/로봇랜디.png",
   },
 ];
 
