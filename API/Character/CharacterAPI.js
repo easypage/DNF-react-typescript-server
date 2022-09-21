c6240395c673ccfad5945701f2c9daf87400b08f
AddVang <3404minib@gmail.com>
2022-09-21 22:54:39 +0900

    server test

--- API/Character/CharacterAPI.js
+++ API/Character/CharacterAPI.js
@@ -1,7 +1,7 @@
 const APIHTTP = "https://api.neople.co.kr/df/servers/";
 const IMAGEHTTP = "https://img-api.neople.co.kr/df/servers/";
 
-const APIKEY = "apikey="+process.env.APIKEY||"apikey=kQbYDpSR4R20Lku7pxRJaDePiOiw0ZpZ";
+const APIKEY = "apikey="+process.env.APIKEY 
 const SearchAPIKey = "?" + APIKEY;
 
 const axios = require("axios");
